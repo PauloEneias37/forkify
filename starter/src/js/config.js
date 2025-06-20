55ee56e13297697c19f0450d589961412eab536a
PauloEneias37 <eneiaspaulo91@gmail.com>
2025-06-20 15:03:50 -0400

    Meu primeiro commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9da9164a-5944-48ca-adcd-3f43d19baddd';
+export const MODAL_CLOSE_SEC = 2.5;
